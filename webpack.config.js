393240bdc0605723819eee6665c6e1cf42745462
SE <se@develup.pro>
2022-04-19 14:22:58 +0300

    alias

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
   resolve: {
     roots: ['./src'],
+    extensions: ['.js', '.jsx'],
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
